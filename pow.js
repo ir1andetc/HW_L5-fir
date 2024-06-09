51b0cf05255fffe20d8d307fb7575168299f0a81
“ir1andetc” <silkin.pavlo@gmail.com>
2024-06-09 13:11:32 +0300

    pow-update

--- pow.js
+++ pow.js
@@ -1,6 +1,24 @@
 function pow(x, y) {
-    let result = 1;
+    // Валідація вхідних даних
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw new Error('Both x and y should be numbers');
+    }
 
+    // Обробка випадків з нульовим ступенем
+    if (y === 0) {
+        if (x === 0) {
+            throw new Error('0 to the power of 0 is undefined');
+        }
+        return 1;
+    }
+
+    // Обробка випадків з від'ємним ступенем
+    if (y < 0) {
+        return 1 / pow(x, -y);
+    }
+
+    // Обчислення степеня для додатних значень y
+    let result = 1;
     for (let i = 0; i < y; i++) {
         result *= x;
     }
@@ -8,4 +26,12 @@ function pow(x, y) {
     return result;
 }
 
-console.log(pow(3, 3)); 
+console.log(pow(3, 3)); // 27
+console.log(pow(2, -2)); // 0.25
+console.log(pow(2, 0)); // 1
+console.log(pow(0, 3)); // 0
+try {
+    console.log(pow(0, 0)); // Error
+} catch (e) {
+    console.error(e.message);
+}
